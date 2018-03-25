52e9352128b4674e55824369243b6cfbe68c165e
Krzysztof Milek <krzysztof.milek73@gmail.com>
2018-03-25 19:49:15 +0200

    Implement REST Api

--- server/routes/lane.routes.js
+++ server/routes/lane.routes.js
@@ -4,4 +4,13 @@ import * as LaneController from '../controllers/lane.controller';
 const router = new Router();
 
 
+// Get all Lanes
+router.route('/lanes').get(LaneController.getLanes);
+
+//Add Lane
+router.route('/lanes').post(LaneController.addLane);
+
+// Delete a lane by laneId
+router.route('/lanes/:laneId').delete(LaneController.deleteLane);
+
 export default router;
